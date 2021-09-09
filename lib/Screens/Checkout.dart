200904fde9a39bc8c8ab2c89743c9e14fd5a251d
your_username <your_email_address@example.com>
2021-09-10 01:44:26 +0500

    Modal Added

--- lib/Screens/Checkout.dart
+++ lib/Screens/Checkout.dart
@@ -134,7 +134,202 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                         ],
                       ),
                     ),
-                    onPressed: () => {},
+                    onPressed: () => {
+                      showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          builder: (context) {
+                            return Padding(
+                              padding: MediaQuery.of(context).viewInsets,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 20.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0, horizontal: 15.0),
+                                          child: Icon(
+                                            Icons.credit_card,
+                                            color: Colors.pink[900],
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 30.0,
+                                          width: 1.0,
+                                          color: Colors.grey.withOpacity(0.5),
+                                          margin: const EdgeInsets.only(
+                                              left: 00.0, right: 10.0),
+                                        ),
+                                        new Expanded(
+                                          child: TextField(
+                                            keyboardType: TextInputType.phone,
+                                            decoration: InputDecoration(
+                                              labelText: 'Card Number',
+                                              border: InputBorder.none,
+                                              hintText: 'Enter 16 digit number',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 20.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0, horizontal: 15.0),
+                                          child: Icon(
+                                            Icons.calendar_today_outlined,
+                                            color: Colors.pink[900],
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 30.0,
+                                          width: 1.0,
+                                          color: Colors.grey.withOpacity(0.5),
+                                          margin: const EdgeInsets.only(
+                                              left: 00.0, right: 10.0),
+                                        ),
+                                        new Expanded(
+                                          child: TextField(
+                                            keyboardType:
+                                                TextInputType.datetime,
+                                            decoration: InputDecoration(
+                                              labelText: 'Valid Until',
+                                              border: InputBorder.none,
+                                              hintText: 'DD/MM/YYYY',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 20.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0, horizontal: 15.0),
+                                          child: Icon(
+                                            Icons.password,
+                                            color: Colors.pink[900],
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 30.0,
+                                          width: 1.0,
+                                          color: Colors.grey.withOpacity(0.5),
+                                          margin: const EdgeInsets.only(
+                                              left: 00.0, right: 10.0),
+                                        ),
+                                        new Expanded(
+                                          child: TextField(
+                                            keyboardType: TextInputType.number,
+                                            decoration: InputDecoration(
+                                              labelText: 'Pin',
+                                              border: InputBorder.none,
+                                              hintText: 'Enter 4 digit pin',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 20.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        new Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0, horizontal: 15.0),
+                                          child: Icon(
+                                            Icons.person,
+                                            color: Colors.pink[900],
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 30.0,
+                                          width: 1.0,
+                                          color: Colors.grey.withOpacity(0.5),
+                                          margin: const EdgeInsets.only(
+                                              left: 00.0, right: 10.0),
+                                        ),
+                                        new Expanded(
+                                          child: TextField(
+                                            keyboardType: TextInputType.name,
+                                            decoration: InputDecoration(
+                                              labelText: 'Card Holder Name',
+                                              border: InputBorder.none,
+                                              hintText:
+                                                  'Enter exact name as per Card',
+                                              hintStyle: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  TextButton(
+                                    style: TextButton.styleFrom(
+                                      textStyle: const TextStyle(fontSize: 15),
+                                    ),
+                                    onPressed: () {},
+                                    child: const Text('Submit'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          })
+                    },
                   ),
                 ),
                 Container(
@@ -223,7 +418,81 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                                 ],
                               ),
                             ),
-                            onPressed: () => {},
+                            onPressed: () => {
+                              showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  builder: (context) {
+                                    return Padding(
+                                      padding:
+                                          MediaQuery.of(context).viewInsets,
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                color: Colors.grey
+                                                    .withOpacity(0.5),
+                                                width: 1.0,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(20.0),
+                                            ),
+                                            margin: const EdgeInsets.symmetric(
+                                                vertical: 10.0,
+                                                horizontal: 20.0),
+                                            child: Row(
+                                              children: <Widget>[
+                                                new Padding(
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 10.0,
+                                                      horizontal: 15.0),
+                                                  child: Icon(
+                                                    Icons.map_outlined,
+                                                    color: Colors.pink[900],
+                                                  ),
+                                                ),
+                                                Container(
+                                                  height: 30.0,
+                                                  width: 1.0,
+                                                  color: Colors.grey
+                                                      .withOpacity(0.5),
+                                                  margin: const EdgeInsets.only(
+                                                      left: 00.0, right: 10.0),
+                                                ),
+                                                new Expanded(
+                                                  child: TextField(
+                                                    keyboardType: TextInputType
+                                                        .streetAddress,
+                                                    decoration: InputDecoration(
+                                                      labelText:
+                                                          'Enter New Address',
+                                                      border: InputBorder.none,
+                                                      hintText:
+                                                          'Complete Billing Address',
+                                                      hintStyle: TextStyle(
+                                                          color: Colors.grey,
+                                                          fontSize: 10),
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                          TextButton(
+                                            style: TextButton.styleFrom(
+                                              textStyle:
+                                                  const TextStyle(fontSize: 15),
+                                            ),
+                                            onPressed: () {},
+                                            child: const Text('Submit'),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  })
+                            },
                           ))
                     ],
                   ),
